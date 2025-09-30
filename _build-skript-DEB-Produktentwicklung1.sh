3d8fba4ba9de63190198086c2239af1a641606a6
Mario Rossdeutscher <mario.rossdeutscher@hs-esslingen.de>
2025-09-30 22:28:26 +0200

    gap gain chapter added

--- _build-skript-DEB-Produktentwicklung1.sh
+++ _build-skript-DEB-Produktentwicklung1.sh
@@ -10,3 +10,10 @@ quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile relea
 quarto render Einleitung_zur_Vorlesung.qmd --to html --profile release-profile -o Einleitung_zur_Vorlesung.html
 
 quarto render Termine.qmd --to html --profile release-profile -o Termine.html
+
+# 2025-09-30-  - probleme mit gap gain - diese 2 funktionieren: 
+
+quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile -o DEB-PE1-Skript_p.html
+# 2025-09-30- geht nur wenn man im Dokument nochmal kommentare off true setzt , das sollte aber eigentlich das release profile machen
+
+quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Skript.html
