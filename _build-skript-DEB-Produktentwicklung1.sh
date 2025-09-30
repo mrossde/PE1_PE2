b30a8ec81b2fc8fa6ccbfd10221e7d821d15ed86
Mario Rossdeutscher <mario.rossdeutscher@hs-esslingen.de>
2025-09-30 07:21:46 +0200

    Termine fixed

--- _build-skript-DEB-Produktentwicklung1.sh
+++ _build-skript-DEB-Produktentwicklung1.sh
@@ -3,6 +3,10 @@
 
 quarto render Labor_Flyer.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Labor01-Flyer.html
 
-quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Labor01-Skript.html
+quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Skript.html
+
+quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile -o DEB-PE1-Skript.html
 
 quarto render Einleitung_zur_Vorlesung.qmd --to html --profile release-profile -o Einleitung_zur_Vorlesung.html
+
+quarto render Termine.qmd --to html --profile release-profile -o Termine.html
