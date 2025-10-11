d37ee820186eeaf9e4a8c6c162a6c8ca9570df42
Mario Rossdeutscher <mario.rossdeutscher@hs-esslingen.de>
2025-10-11 21:54:26 +0200

    flyer solutions updated

--- _build-skript-DEB-Produktentwicklung1.sh
+++ _build-skript-DEB-Produktentwicklung1.sh
@@ -3,6 +3,11 @@
 
 quarto render Labor_Flyer.qmd --to html --profile release-profile -o DEB-PE1-Labor01-Flyer.html
 
+quarto render Labor_Flyer.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Labor01-Flyer_m_Loesung.html
+
+
+
+
 quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile,wip-profile -o DEB-PE1-Skript.html
 
 quarto render _DEB3_Vorlesung_Produktentwicklung_1.qmd --to html --profile release-profile -o DEB-PE1-Skript.html
