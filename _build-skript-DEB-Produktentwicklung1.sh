2e6467c71ace6d8a5606f35d2d7cde0aca580a6c
Mario Rossdeutscher <mario.rossdeutscher@hs-esslingen.de>
2025-11-18 10:12:58 +0100

    starship detail

--- _build-skript-DEB-Produktentwicklung1.sh
+++ _build-skript-DEB-Produktentwicklung1.sh
@@ -1,6 +1,11 @@
 
 #quarto render 00010-Projektmanagement.qmd --to html --profile release-profile, cpp-profile -o MTB-Informationstechnik-00010-Projektmanagement.html
 
+
+quarto render Aufgabe_Starship-Loesung.qmd --to html --profile release-profile -o Aufgabe_Starship-lsg.html
+
+quarto render Aufgabe_Starship.qmd --to html --profile release-profile -o Aufgabe_Starship.html
+
 quarto render Labor_Flyer.qmd --to html --profile release-profile -o DEB-PE1-Labor01-Flyer.html
 
 quarto render Bewertungsmatrix.qmd --to html --profile release-profile -o DEB-PE1-BMatrix-einzeln.html
